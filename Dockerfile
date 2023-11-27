c7be5c1bc52fbf50828bbe7f1ba3510e5b74983f
Aman Agarwal <amanagarwal@Aditis-MacBook-Pro.local>
2023-11-27 11:26:43 +0530

    feat(docker): fixing docker issue

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,7 @@ COPY . .
 # Build the Next.js app
 RUN npm run build
 
+COPY  /.next/standalone ./
 # Expose the desired port
 EXPOSE 3000
 
--- src/services/services.ts
+++ src/services/services.ts
@@ -2,24 +2,23 @@ import axios from 'axios';
 
 import { SurveyResponseType } from '@/type/type';
 
+const backendUrl =
+  process.env.WPCAS_SERVICE_BACKEND_IP || 'http://localhost:4010';
 export const getUsers = async (assesseeId: string) => {
   const data = await axios.get(
-    `http://localhost:3000/api/response-tracker/assessor/${assesseeId}`
+    `${backendUrl}/api/response-tracker/assessor/${assesseeId}`
   );
   return data.data.data;
 };
 
 export const saveResponse = async (payload: SurveyResponseType) => {
-  const data = await axios.patch(
-    'http://localhost:3000/api/response-tracker',
-    payload
-  );
+  const data = await axios.patch('${backendUrl}/api/response-tracker', payload);
   return data.data.data;
 };
 
 export const getSurveyConfigQuestionById = async (id: string) => {
   const data = await axios(
-    `http://localhost:3000/api/survey-form/latest-survey-form/${id}`
+    `${backendUrl}/api/survey-form/latest-survey-form/${id}`
   );
   return data.data.data;
 };
