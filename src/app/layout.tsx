dc61fdfc2b5fe49f1d26b44e5c188f30ae01b21e
faisalEsMagico <faisal.khan@esmagico.in>
2023-12-15 23:43:21 +0530

    feat(meta): meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,41 +10,38 @@ import 'react-toastify/dist/ReactToastify.css';
 import ConnectionCheckWrapper from '@/components/errorScreen/ConnectionCheckWrapper';
 import PullRefresh from '@/components/pullRefresh/PullRefresh';
 
-import { siteConfig } from '@/constant/config';
-
 // !STARTERCONF Change these default meta
 // !STARTERCONF Look at @/constant/config to change them
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.title,
-    template: `%s | ${siteConfig.title}`,
-  },
-  description: siteConfig.description,
+  title: 'Compass Wpcas-survey demo',
+
+  description:
+    'Introducing Wpcas-survey, a platform where assessors and assesses can provide and receive feedback tailored to their competency',
   robots: { index: true, follow: true },
   // !STARTERCONF this is the default favicon, you can generate your own from https://realfavicongenerator.net/
   // ! copy to /favicon folder
   icons: {
-    icon: '/favicon/favicon.ico',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
+    icon: '/favicon/samagra_logo.jpg',
+    shortcut: '/favicon/samagra_logo.jpg',
+    apple: '/favicon/samagra_logo.jpg',
   },
-  manifest: `/favicon/site.webmanifest`,
-  openGraph: {
-    url: siteConfig.url,
-    title: siteConfig.title,
-    description: siteConfig.description,
-    siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
-    type: 'website',
-    locale: 'en_US',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.title,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/images/og.jpg`],
-    // creator: '@th_clarence',
-  },
+  // manifest: `/favicon/site.webmanifest`,
+  // openGraph: {
+  //   url: siteConfig.url,
+  //   title: siteConfig.title,
+  //   description: siteConfig.description,
+  //   siteName: siteConfig.title,
+  //   images: [`${siteConfig.url}/images/og.jpg`],
+  //   type: 'website',
+  //   locale: 'en_US',
+  // },
+  // twitter: {
+  //   card: 'summary_large_image',
+  //   title: siteConfig.title,
+  //   description: siteConfig.description,
+  //   images: [`${siteConfig.url}/images/og.jpg`],
+  //   // creator: '@th_clarence',
+  // },
   // authors: [
   //   {
   //     name: 'Theodorus Clarence',
